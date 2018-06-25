98ba7ade3bb16a1befd66b94f02962951df7187d
U-PROCESDATA\pm <pm@PM-PC-1.proces-data.com>
2018-06-25 08:10:35 +0200

    First take on NTP server/client

--- apps/ntpd/ntpd.c
+++ apps/ntpd/ntpd.c
@@ -34,6 +34,8 @@
 #define PRINTF(...)
 #endif
 
+#define UIP_IP_BUF   ((struct uip_ip_hdr *)&uip_buf[UIP_LLH_LEN])
+
 static unsigned short SEND_INTERVAL = 60 * 60; // every hour
 static unsigned long StartTime = 0;
 static unsigned long CurrTime = 0;
@@ -45,6 +47,7 @@ PROCESS(ntpd_process, "ntpd");
 
 static struct ctimer delay_timer;
 volatile uint8_t NTP_BUSY = 0;
+volatile uint8_t current_stranum = 15;
 
 static void
 notify_ready(void *not_used)
@@ -75,19 +78,29 @@ static void tcpip_handler(void)
 
 		switch(NTP->mode)
 		{
-			case 4:
-				CurrTime = uip_ntohl(NTP->tx_Time_s) - NTP_EPOCH; //UNIX time
-				clock_set_unix_time(CurrTime);
-				StartTime = clock_seconds();
+			case 4: // We got a reply for the request
+				if(current_stranum > NTP->stratum )
+				{
+					CurrTime = uip_ntohl(NTP->tx_Time_s) - NTP_EPOCH; //UNIX time
+					clock_set_unix_time(CurrTime);
+					StartTime = clock_seconds();
+					current_stranum = (NTP->stratum + 1)>15?15:(NTP->stratum + 1);
+				}
 				// We got response so NTP is happy
 				NTP_BUSY = 0;
 				break;
 
-			case 3:
+			case 3: // Someone is asking us for the time
 				time = clock_get_unix_time();
 				if(time){
+					NTP_server.stratum = current_stranum;
 					NTP_server.tx_Time_s = uip_ntohl(time + NTP_EPOCH);
-					uip_udp_packet_send(ntp_conn, &NTP_server, 48);
+
+					uip_ipaddr_copy(&ntp_conn->ripaddr, &UIP_IP_BUF->srcipaddr);
+
+					uip_udp_packet_send(ntp_conn,  &NTP_server, 48);
+					// Restore server connection to allow data from any node
+					uip_ipaddr_copy(&ntp_conn->ripaddr, uip_ds6_defrt_choose());
 					// Wait 50 ms so we have time to send packet
 					ctimer_set(&delay_timer, 50, notify_ready, NULL);
 					NTP_BUSY = 1;
@@ -100,12 +113,28 @@ static void tcpip_handler(void)
 	}
 }
 /*---------------------------------------------------------------------------*/
-static void timeout_handler(void)
+static void Send_NTP_request(void)
 {
+	// Request time
 	uip_udp_packet_send(ntp_conn, &ntpmsg, 48);
 	// Wait 50 ms for response
 	ctimer_set(&delay_timer, 50, notify_ready, NULL);
 	NTP_BUSY = 1;
+}
+/*---------------------------------------------------------------------------*/
+static void Send_NTP_time_to_parrent(void)
+{
+	ntp_packet_t NTP_server = { .li = 0, .ver = 3, .mode = 4, 0 };
+	clock_time_t time;
+	time = clock_get_unix_time();
+	if(time){
+		NTP_server.stratum = current_stranum;
+		NTP_server.tx_Time_s = uip_ntohl(time + NTP_EPOCH);
+		uip_udp_packet_send(ntp_conn, &NTP_server, 48);
+		// Wait 50 ms so we have time to send packet
+		ctimer_set(&delay_timer, 50*CLOCK_SECOND/1000, notify_ready, NULL);
+		NTP_BUSY = 1;
+	}
 }
 /*---------------------------------------------------------------------------*/
 /*static void
@@ -117,6 +146,7 @@ static void timeout_handler(void)
 PROCESS_THREAD(ntpd_process, ev, data)
 {
 	static struct etimer et;
+	static struct etimer Update_parrent_timer;
 	static uip_ipaddr_t ipaddr;
 
 	PROCESS_BEGIN();
@@ -139,12 +169,16 @@ PROCESS_THREAD(ntpd_process, ev, data)
 	ntp_conn = udp_new(&ipaddr, UIP_HTONS(NTPD_PORT), NULL);
 
 	etimer_set(&et, SEND_INTERVAL * CLOCK_SECOND);
+	etimer_set(&Update_parrent_timer, 60 * CLOCK_SECOND);
 	while(1){
 		PROCESS_YIELD();
 		if(etimer_expired(&et)){
-			timeout_handler();
+			Send_NTP_request();
 			etimer_set(&et, SEND_INTERVAL * CLOCK_SECOND);
-		} else if(ev == tcpip_event){
+		}else if(etimer_expired(&Update_parrent_timer)){
+			Send_NTP_time_to_parrent();
+			etimer_set(&Update_parrent_timer, 60 * CLOCK_SECOND);
+		}else if(ev == tcpip_event){
 			tcpip_handler();
 		}
 	}
